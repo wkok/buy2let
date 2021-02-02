555bcce9ee6091eab105b272998a956ccc95e50e
wkok <wernerkok@gmail.com>
2021-02-02 14:56:53 +0000

    Updated version

--- src/cljs/wkok/buy2let/about/views.cljs
+++ src/cljs/wkok/buy2let/about/views.cljs
@@ -27,7 +27,7 @@
     [grid {:item true}
      [list
       [list-item {:button true}
-       [list-item-text {:primary "Version" :secondary "0.0.7"}]]
+       [list-item-text {:primary "Version" :secondary "0.0.8"}]]
       [list-item {:button true
                   :component "a"
                   :href (mm/terms-of-service-url)
