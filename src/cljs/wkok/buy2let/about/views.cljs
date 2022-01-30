67c59face510992db87e10babcbbe66b61ae6b67
Werner Kok <wernerkok@gmail.com>
2022-01-30 19:37:06 +0000

    bump version

--- src/cljs/wkok/buy2let/about/views.cljs
+++ src/cljs/wkok/buy2let/about/views.cljs
@@ -17,7 +17,7 @@
     [grid {:item true}
      [list
       [list-item {:button true}
-       [list-item-text {:primary "Version" :secondary "0.2.3"}]]
+       [list-item-text {:primary "Version" :secondary "0.2.4"}]]
       [list-item {:button true
                   :component "a"
                   :href (mm/terms-of-service-url)
